019a88c4daee8df06d18ecdefcdf2bd947211495
ngoclong-fulbright <69509154+ngoclong-fulbright@users.noreply.github.com>
2022-03-12 22:48:17 +0700

    lobby page draft

---
+++ lib/consts/app_styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const triviaHeading1 = TextStyle(
+  color: Colors.pinkAccent,
+  fontWeight: FontWeight.w800,
+  fontFamily: 'Roboto',
+  letterSpacing: 0.6,
+  fontSize: 40,
+  height: 2,
+  decoration: TextDecoration.none
+);
+
+const triviaHeading2 = TextStyle(
+  color: Colors.pinkAccent,
+  fontWeight: FontWeight.w800,
+  fontFamily: 'Roboto',
+  letterSpacing: 0.6,
+  fontSize: 30,
+  height: 2,
+  decoration: TextDecoration.none
+);
--- lib/views/pages/lobby_page.dart
+++ lib/views/pages/lobby_page.dart
@@ -1,1 +1,90 @@
-// waiting room
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../consts/app_styles.dart';
+
+class LobbyPage extends StatelessWidget{
+  const LobbyPage ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        Expanded(
+            flex: 2,
+            child: WaitingInfo()
+        ),
+        Expanded(
+          flex: 1,
+            child: RulesNav()
+        ),
+        Expanded(
+            child: GuidelineNav()
+        ),
+      ],
+    );
+  }
+}
+
+class WaitingInfo extends StatelessWidget{
+  const WaitingInfo ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.deepPurple
+      ),
+      padding: const EdgeInsets.only(left: 50, right: 50),
+      child: const Center(
+        child: Text("Waiting for host to start the game...",
+          style: triviaHeading2,
+          textAlign: TextAlign.center,
+        )
+      )
+    );
+  }
+}
+
+class RulesNav extends StatelessWidget{
+  const RulesNav ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: const BoxDecoration(
+            color: Colors.amberAccent
+        ),
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: Center(
+            child: TextButton(
+                onPressed: (){},
+                child: const Text("Game rules",
+                    style: triviaHeading1,
+                    textAlign: TextAlign.center,
+                    )
+            )
+        )
+    );
+  }
+}
+
+class GuidelineNav extends StatelessWidget{
+  const GuidelineNav ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+  return Container(
+      decoration: const BoxDecoration(
+          color: Colors.cyanAccent
+      ),
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: const Center(
+          child: Text("App guidelines",
+            style: triviaHeading1,
+            textAlign: TextAlign.center,
+          )
+      )
+  );
+  }
+}
