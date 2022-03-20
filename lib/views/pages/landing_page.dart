d60ca8419b69a9e7369464f9e7e65eba95c2e763
ngoclong-fulbright <69509154+ngoclong-fulbright@users.noreply.github.com>
2022-03-20 21:25:17 +0700

    revert test

--- lib/views/pages/landing_page.dart
+++ lib/views/pages/landing_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:trivia_app/consts/app_styles.dart';
 import 'package:trivia_app/views/pages/team_formation_page.dart';
 import 'package:trivia_app/views/widgets/TextFieldWithButton.dart';
-import 'package:trivia_app/views/widgets/test.dart';
 
 class LandingPage extends StatelessWidget {
   static const routeName = "/";
