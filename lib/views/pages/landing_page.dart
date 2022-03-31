0e621b0f163deb893d5b24d849b126f2922045a5
ttnhuy313 <39652384+ttnhuy313@users.noreply.github.com>
2022-03-31 09:55:43 +0700

    Test animation

--- lib/views/pages/landing_page.dart
+++ lib/views/pages/landing_page.dart
@@ -1,39 +1,70 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:trivia_app/consts/app_styles.dart';
 import 'package:trivia_app/views/pages/team_formation_page.dart';
 import 'package:trivia_app/views/widgets/TextFieldWithButton.dart';
-import 'package:trivia_app/views/widgets/test.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-class LandingPage extends StatelessWidget {
+class LandingPage extends StatefulWidget {
   static const routeName = "/";
-
   const LandingPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final isKeyboard = MediaQuery.of(context).viewInsets.bottom != 0;
+  State<LandingPage> createState() => _State();
+}
 
-    return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      body: Column(
-        children: [
-          Expanded(
-              flex: 2,
-              child: Transform.scale(
-                  scale: 1.2, child: Image.asset('assets/images/welcome.png'))),
-          Expanded(
-              flex: 1,
-              child: Align(
-                alignment: Alignment.topCenter,
-                child: TextFieldWithButton(
-                    routeName: TeamFormationPage.routeName,
-                    isKeyboard: isKeyboard),
-              )),
-        ],
-      ),
+class _State extends State<LandingPage> with SingleTickerProviderStateMixin {
+
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 10),
     );
+    _animationController.repeat();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isKeyboard = MediaQuery
+        .of(context)
+        .viewInsets
+        .bottom != 0;
+
+    return SafeArea(child: Stack(children: <Widget>[
+      AnimatedBuilder(
+          animation: _animationController,
+          child: Image.asset('assets/images/sky.jpg'),
+          builder: (context, child) =>
+              Transform.scale(
+                scale: _animationController.value,
+                child: child,
+              )),
+
+      Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Column(
+          children: [
+            Expanded(
+                flex: 2,
+                child: Transform.scale(
+                    scale: 1.2,
+                    child: Image.asset('assets/images/welcome.png'))),
+            Expanded(
+                flex: 1,
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: TextFieldWithButton(
+                      routeName: TeamFormationPage.routeName,
+                      isKeyboard: isKeyboard),
+                )),
+          ],
+        ),
+      ),
+      const Align(alignment: Alignment.center,
+          child: Text('Hello', style: triviaHeading1)),
+    ]));
   }
 }
