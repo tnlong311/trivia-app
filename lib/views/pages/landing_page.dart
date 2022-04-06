aa3bc3ea1ef076e06822717d8448b60b3403fee1
ngoclong-fulbright <69509154+ngoclong-fulbright@users.noreply.github.com>
2022-04-06 16:38:01 +0700

    reformat TextFieldWithButton

--- lib/views/pages/landing_page.dart
+++ lib/views/pages/landing_page.dart
@@ -5,9 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:trivia_app/consts/app_styles.dart';
 import 'package:trivia_app/services/auth_service.dart';
 import 'package:trivia_app/views/pages/team_formation_page.dart';
-import 'package:trivia_app/views/widgets/SignInTextField.dart';
 
 import '../widgets/Layer.dart';
+import '../widgets/TextFieldWithButton.dart';
 
 class LandingPage extends StatefulWidget {
   static const routeName = "/";
@@ -29,7 +29,7 @@ class _LandingPageState extends State<LandingPage> {
     super.dispose();
   }
 
-  bool _isLoggedIn = false;
+  // bool _isLoggedIn = false;
 
   validateSignIn(value) async {
     var pin = await AuthService.signIn(value);
@@ -43,23 +43,25 @@ class _LandingPageState extends State<LandingPage> {
   }
 
   inputValidator(value) {
-    print(_isLoggedIn);
+    // print(_isLoggedIn);
     if (value == null || value.isEmpty) {
       return 'Please enter some text';
-    } else if (!_isLoggedIn) {
-      return 'Wrong game code';
     }
+    // else if (!_isLoggedIn) {
+    //   return 'Wrong game code';
+    // }
 
     return null;
   }
 
   inputUpdator(value) async {
-    if (value != '' && value != null) {
-      await validateSignIn(value).then((status) {
-        if (status) {
-          _isLoggedIn = true;
-        }
-      });
+    var pin = await AuthService.signIn(value);
+
+    if (pin == null || pin == '') {
+      return false;
+    } else {
+      print(pin);
+      return true;
     }
   }
 
@@ -92,10 +94,12 @@ class _LandingPageState extends State<LandingPage> {
                 flex: 1,
                 child: Align(
                   alignment: Alignment.topCenter,
-                  child: SignInTextField(
+                  child: TextFieldWithButton(
                       validator: inputValidator,
                       updator: inputUpdator,
                       routeName: TeamFormationPage.routeName,
+                      failMsg: 'Wrong game code!',
+                      successMsg: 'Game joined!',
                       isKeyboard: isKeyboard),
                 ))
           ]),
--- lib/views/widgets/custom_snackbar.dart
+++ lib/views/widgets/custom_snackbar.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CustomSnackBar {
+  static const double height = 20;
+  static const double widthPercentage = 0.8;
+
   static void showSuccessSnackBar(BuildContext context, String text) {
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text(text),
+      content: Container(
+          alignment: Alignment.center,
+          height: height,
+          width: MediaQuery.of(context).size.width*widthPercentage,
+          child: Text(text)),
       duration: const Duration(milliseconds: 1500),
       margin: const EdgeInsets.all(20),
       shape: const RoundedRectangleBorder(
@@ -15,7 +22,11 @@ class CustomSnackBar {
 
   static void showFailSnackBar(BuildContext context, String text) {
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text(text),
+      content: Container(
+          alignment: Alignment.center,
+          height: height,
+          width: MediaQuery.of(context).size.width*widthPercentage,
+          child: Text(text)),
       duration: const Duration(milliseconds: 1500),
       margin: const EdgeInsets.all(20),
       shape: const RoundedRectangleBorder(
